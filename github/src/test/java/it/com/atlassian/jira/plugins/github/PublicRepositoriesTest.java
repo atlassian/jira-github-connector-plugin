5f1d2170d5fd5fe2c7394342e956d7e79aacc9f5
Federico Silva Armas <fsilva.armas@gmail.com>
2011-06-03 17:19:45 +1000

    Adding integration tests

--- github/src/test/java/it/com/atlassian/jira/plugins/github/PublicRepositoriesTest.java
+++ github/src/test/java/it/com/atlassian/jira/plugins/github/PublicRepositoriesTest.java
@@ -1,26 +1,45 @@
 package it.com.atlassian.jira.plugins.github;
 
-import com.atlassian.jira.plugins.github.pageobjects.GitHubConfigureRepositoriesPage;
-import com.atlassian.pageobjects.TestedProductFactory;
-import com.atlassian.pageobjects.page.LoginPage;
-import com.atlassian.pageobjects.page.WebSudoPage;
-import com.atlassian.webdriver.jira.JiraTestedProduct;
+import com.atlassian.jira.plugins.github.pageobjects.component.GitHubCommitEntry;
+import com.atlassian.jira.plugins.github.pageobjects.page.JiraViewIssuePage;
 import org.junit.Test;
 
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
 /**
- * TODO: Document this class / interface here
+ * Test to verify behaviour when syncing public github repos.
  */
-public class PublicRepositoriesTest
+public class PublicRepositoriesTest extends GitHubBaseTest
 {
     @Test
-    public void addPublicRepo()
+    public void addPublicRepo_VerifyAppearsInList()
     {
-        JiraTestedProduct jira = TestedProductFactory.create(JiraTestedProduct.class);
-
-        GitHubConfigureRepositoriesPage configureRepos = jira.visit(LoginPage.class)
-                                                             .loginAsSysAdmin(WebSudoPage.class)
-                                                             .confirm(GitHubConfigureRepositoriesPage.class);
-
+        configureRepos.deleteAllRepositories();
         
+        configureRepos.addPublicRepoToProject("QA", "https://github.com/farmas/TestRepo-QA");
+
+        assertEquals(1, configureRepos.getRepositories().size());
+    }
+
+    @Test
+    public void addPublicRepo_VerifyCommitsOnIssues()
+    {
+        if(configureRepos.isRepositoryPresent("QA", "https://github.com/farmas/TestRepo-QA/master") == false)
+        {
+            configureRepos.addPublicRepoToProject("QA", "https://github.com/farmas/TestRepo-QA");
+        }
+
+        List<GitHubCommitEntry> commitList = jira.visit(JiraViewIssuePage.class, "QA-1")
+                                                          .openGitHubPanel()
+                                                          .waitForMessages();
+
+        assertTrue("Expected at least 1 commit entry", commitList.size() > 1);
+
+        //verify message of first commit
+        assertEquals("GH add 1 file to QA-1", commitList.get(commitList.size()-1).getCommitMessage());
+
     }
 }
